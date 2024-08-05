89eb36408b722ec4d9cf41b94718d5a44b3c5730
karthik gurnani <118505109+karthik393@users.noreply.github.com>
2024-08-05 13:31:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 const passwordBox = document.getElementById("password");
 
+// adding password's input id here
+
 const length = 12;
+
+// declraing the length of the password and without adding a string
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -13,24 +17,36 @@ const symbols = "@#$%^&*()-_+~={}[]<>/";
 
 const allChars = upperCase + lowerCase + numbers + symbols;
 
+// declraing all the characters used for genrating pass
+
 function createpassword() {
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += numbers[Math.floor(Math.random() * numbers.length)];
     password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    // making an empty string and then making it += each pass const with math floor,random and 
+    // .length is used to track the numbers in password string. means length of a string i.e password var
+
+while (password.length < length){
+    password += allChars[Math.floor(Math.random() * allChars.length)];
 }
 
-while (length > length.password){
-    password += allChars[Math.floor(Math.random() * allChars.length)];
-
+// this while loop is used to execute as long as the password string is as diseired as 12 char i.e less than
+// when the statement is true the code inside the curly braces will work again and again. until the statement is no longer true. 
 
 passwordBox.value = password;
 
+// adding password to passwordbox's input
 }
-
-createpassword();
 
 document.querySelector(".generate-btn").addEventListener("click", function(){
   createpassword();
+  
+});
+
+document.querySelector(".copy").addEventListener("click", function(){
+   passwordBox.select();
+   navigator.clipboard.writeText(passwordBox.value);
 });
